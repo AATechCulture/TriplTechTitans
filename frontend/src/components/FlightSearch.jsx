81da705cb0ee80ef1aa31aa0e74790810cf047e0
Arnold Bhebhe <legendaryarnoldb@gmail.com>
2023-11-05 01:53:38 -0500

    update search form

--- frontend/src/components/FlightSearch.jsx
+++ frontend/src/components/FlightSearch.jsx
@@ -1,40 +1,58 @@
 import { useState } from "react";
-// import "./FlightSearch.css";
-import { TextField, Button, Typography, Box } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Typography,
+  Box,
+  Grid,
+  InputAdornment,
+} from "@mui/material";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./FlightSearch.css";
 
 const FlightSearch = ({ searchFlights }) => {
-  const [origin, setOrigin] = useState("");
-  const [destination, setDestination] = useState("");
-  const [departureDate, setDepartureDate] = useState(null);
-  const [returnDate, setReturnDate] = useState(null);
+  const [formData, setFormData] = useState({
+    origin: "",
+    destination: "",
+    departureDate: null,
+    returnDate: null,
+    numAdults: 1,
+  });
 
   const onInputChange = (e) => {
-    switch (e.target.id) {
-      case "origin":
-        setOrigin(e.target.value);
-        break;
-      case "destination":
-        setDestination(e.target.value);
-        break;
-      default:
-        break;
-    }
+    const { id, value } = e.target;
+    setFormData({
+      ...formData,
+      [id]: value,
+    });
+  };
+
+  const onDateChange = (date, field) => {
+    setFormData({
+      ...formData,
+      [field]: date,
+    });
   };
 
   const validateInputs = () => {
-    return origin && destination && departureDate && returnDate;
+    return (
+      formData.origin &&
+      formData.destination &&
+      formData.departureDate &&
+      formData.returnDate &&
+      formData.numAdults > 0
+    );
   };
 
   const onSearchFlights = () => {
     if (validateInputs()) {
       searchFlights(
-        origin,
-        destination,
-        departureDate.format("YYYY-MM-DD"),
-        returnDate.format("YYYY-MM-DD")
+        formData.origin,
+        formData.destination,
+        formData.departureDate.format("YYYY-MM-DD"),
+        formData.returnDate.format("YYYY-MM-DD"),
+        formData.numAdults
       );
     }
   };
@@ -44,35 +62,80 @@ const FlightSearch = ({ searchFlights }) => {
       <Typography variant="h4" sx={{ mb: 5 }}>
         Search for Routes
       </Typography>
-      <div className="fs-container">
-        <TextField
-          id="origin"
-          label="Origin"
-          variant="outlined"
-          value={origin}
-          onChange={onInputChange}
-        />
-        <TextField
-          id="destination"
-          label="Destination"
-          variant="outlined"
-          value={destination}
-          onChange={onInputChange}
-        />
-        <DatePicker
-          placeholderText="Departure date"
-          selected={departureDate}
-          onChange={setDepartureDate}
-        />
-        <DatePicker
-          placeholderText="Return date"
-          selected={returnDate}
-          onChange={setReturnDate}
-        />
-        <Button variant="contained" color="primary" onClick={onSearchFlights}>
-          {"Search"}
-        </Button>
-      </div>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          alignContent: "center",
+          alignItems: "center",
+          justifyContent: "center",
+          justifyItems: "center",
+        }}
+      >
+        <Grid item xs={12} sm={2}>
+          <TextField
+            fullWidth
+            id="origin"
+            label="Origin"
+            variant="outlined"
+            value={formData.origin}
+            onChange={onInputChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={2}>
+          <TextField
+            fullWidth
+            id="destination"
+            label="Destination"
+            variant="outlined"
+            value={formData.destination}
+            onChange={onInputChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={2}>
+          <DatePicker
+            selected={formData.departureDate}
+            onChange={(date) => onDateChange(date, "departureDate")}
+            placeholderText="Departure date"
+            className="date-picker"
+          />
+        </Grid>
+        <Grid item xs={12} sm={2}>
+          <DatePicker
+            selected={formData.returnDate}
+            onChange={(date) => onDateChange(date, "returnDate")}
+            placeholderText="Return date"
+            className="date-picker"
+          />
+        </Grid>
+        <Grid item xs={12} sm={2}>
+          <TextField
+            fullWidth
+            type="number"
+            id="numAdults"
+            label="Number of Adults"
+            variant="outlined"
+            value={formData.numAdults}
+            onChange={onInputChange}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">Adults</InputAdornment>
+              ),
+              inputProps: { min: 1 },
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={2}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={onSearchFlights}
+            disabled={!validateInputs()}
+          >
+            {"Search"}
+          </Button>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
